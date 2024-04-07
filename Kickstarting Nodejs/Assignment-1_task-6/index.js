1585d1c526cc5690ade1e6591234ce05de94cee2
Suman <sumankamilya27@gmail.com>
2024-04-07 18:49:45 +0530

    task - 8

--- Kickstarting Nodejs/Assignment-1_task-6/index.js
+++ Kickstarting Nodejs/Assignment-1_task-6/index.js
@@ -1,19 +1,56 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome</h1>");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const messagesFilePath = path.join(process.cwd(), 'messages.txt');
+
+function readMessagesFromFile() {
+  try {
+      const data = fs.readFileSync(messagesFilePath, 'utf8');
+      return data.split('\n').filter(message => message.trim() !== '');
+  } catch (err) {
+      console.error("Error reading messages from file:", err);
+      return [];
+  }
+}
+
+
+function writeMessageToFile(message) {
+  try {
+      fs.appendFileSync(messagesFilePath, message + '\n');
+  } catch (err) {
+      console.error("Error writing message to file:", err);
+  }
+}
+
+
+app.get('/', (req, res) => {
+  const messages = readMessagesFromFile();
+  res.send(`
+      <form method="post" action="/">
+          <input type="text" name="message" placeholder="Enter your message">
+          <button type="submit">Submit</button>
+      </form>
+      <div>
+          <h2>Messages:</h2>
+          ${messages.map(message => `<p>${message}</p>`).join('')}
+      </div>
+  `);
 });
-app.get("/home", (req, res) => {
-  res.send("<h1>welcome home</h1>");
+
+app.post('/', (req, res) => {
+  const message = req.body.message;
+  if (message) {
+      writeMessageToFile(message);
+  }
+  res.redirect('/');
 });
-app.get("/node", (req, res) => {
-  res.send("<h1>welcome to my node Js project</h1>");
-});
-app.get("/about", (req, res) => {
-  res.send("<h1>Welcome to About us page</h1>");
-});
+
 
 app.listen(4000, () => {
   console.log("App is listening at http://localhost:4000");
